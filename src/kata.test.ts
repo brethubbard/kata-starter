4911aa28148048457994c15d697309c231e346e2
Bret Hubbard <bhubbard@marq.com>
2023-09-01 07:22:05 -0600

    solution through custom delimiter

--- src/kata.test.ts
+++ src/kata.test.ts
@@ -1,2 +1,28 @@
 import { describe, expect, test, it } from "@jest/globals";
 import { add } from "./kata";
+
+describe("add", () => {
+  it("should return 0 when input is empty", () => {
+    expect(add("")).toBe(0);
+  });
+
+  it("should return 1 when input is 1", () => {
+    expect(add("1")).toBe(1);
+  });
+
+  it("should return 3 when input is 1,2", () => {
+    expect(add("1,2")).toBe(3);
+  });
+
+  it("should return 6 when input is 1,2,3", () => {
+    expect(add("1,2,3")).toBe(6);
+  });
+
+  it("should return 6 when input is 1\n2,3", () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
+
+  it("should return 3 when input is //;\n1;2", () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });
+});
--- src/kata.ts
+++ src/kata.ts
@@ -1,1 +1,20 @@
-export function add(input) {}
+export function add(input): number {
+  const [delimiter, numbers] = parseInput(input);
+
+  const parsedNumbers = parseNumbers(delimiter, numbers);
+
+  return parsedNumbers.reduce((acc, n) => acc + n, 0);
+}
+
+function parseInput(input: string): [string | RegExp, string] {
+  if (input.startsWith("//")) {
+    const [delimiter, numbers] = input.split("\n");
+    return [delimiter.slice(2), numbers];
+  }
+
+  return [/[\,\n]/, input];
+}
+
+function parseNumbers(delimiter: string | RegExp, numbers: string): number[] {
+  return numbers.split(delimiter).map((n) => +n);
+}
